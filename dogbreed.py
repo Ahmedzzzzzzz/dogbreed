4cb41f5c3a778ec56edd4905e74219ae48dc46d0
Ahmedzzzzzzz <ahmedfun450@gmail.com>
2025-06-02 22:11:47 +0300

    Update dogbreed.py

---
+++ dogbreed.py
@@ -0,0 +1,14 @@
+animal = "dog"
+breed = "pitbul"
+colour = "black"
+
+print("animal is", animal)
+print("breed is", breed)
+print("colour is", colour)
+
+breed2 = "poodle"
+colour2 = "white"
+
+print("animal is", animal)
+print("breed is", breed2)
+print("colour is", colour2)
